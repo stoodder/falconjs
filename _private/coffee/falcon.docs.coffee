ad54b8e2b36074f1bada8c2320c175ee56970db3
Rick Allen <stoodder@gmail.com>
2013-09-11 13:33:44 -0500

    Added console logger

--- _private/coffee/falcon.docs.coffee
+++ _private/coffee/falcon.docs.coffee
@@ -1,17 +1,32 @@
-
+window.logger = (logger_id, value) ->
+	console.log( value )
+	if (value instanceof Array) and (first_value = value[0])
+		if Falcon.isModel( first_value ) and first_value.get("name")?
+			value = ( v.get("name") for v in value )
+		#END if
+	#END if
+	$("##{logger_id}").append("<div>#{value}</div>").toggleClass("visible", true)
+#END logger
 $ ->
+	logger_index = 0
+
 	$("code").each (index, element) ->
 		$element = $(element)
 		html = $element.html().replace(/\&lt;/gi, "<").replace(/\&gt;/gi, ">")
-		$element.click -> console.log( html );
 		$element.text( html )
-
 
 		if $element.hasClass("runnable")
 			code = $element.text()
+			logger_id = "__logger_#{logger_index++}__"
+			code = code.replace(/console\.log\(/gi, "logger('#{logger_id}',")
 			$runner = $("<div class='runner'><i class='glyphicon glyphicon-play'></div>")
+			$logger = $("<div class='logger' id='#{logger_id}'></div>")
 			$element.append($runner)
+			$element.append($logger)
+
 			$runner.on "click", ->
+				$(@).siblings(".logger").empty();
+
 				_ajax = $.ajax
 				$.ajax = (->)
 
--- _site/scripts/falcon.docs.js
+++ _site/scripts/falcon.docs.js
@@ -1,21 +1,47 @@
 (function() {
+  window.logger = function(logger_id, value) {
+    var first_value, v;
+
+    console.log(value);
+    if ((value instanceof Array) && (first_value = value[0])) {
+      if (Falcon.isModel(first_value) && (first_value.get("name") != null)) {
+        value = (function() {
+          var _i, _len, _results;
+
+          _results = [];
+          for (_i = 0, _len = value.length; _i < _len; _i++) {
+            v = value[_i];
+            _results.push(v.get("name"));
+          }
+          return _results;
+        })();
+      }
+    }
+    return $("#" + logger_id).append("<div>" + value + "</div>").toggleClass("visible", true);
+  };
+
   $(function() {
+    var logger_index;
+
+    logger_index = 0;
     return $("code").each(function(index, element) {
-      var $element, $runner, code, html;
+      var $element, $logger, $runner, code, html, logger_id;
 
       $element = $(element);
       html = $element.html().replace(/\&lt;/gi, "<").replace(/\&gt;/gi, ">");
-      $element.click(function() {
-        return console.log(html);
-      });
       $element.text(html);
       if ($element.hasClass("runnable")) {
         code = $element.text();
+        logger_id = "__logger_" + (logger_index++) + "__";
+        code = code.replace(/console\.log\(/gi, "logger('" + logger_id + "',");
         $runner = $("<div class='runner'><i class='glyphicon glyphicon-play'></div>");
+        $logger = $("<div class='logger' id='" + logger_id + "'></div>");
         $element.append($runner);
+        $element.append($logger);
         return $runner.on("click", function() {
           var aUrl, tUrl, _ajax;
 
+          $(this).siblings(".logger").empty();
           _ajax = $.ajax;
           $.ajax = (function() {});
           aUrl = Falcon.baseApiUrl;
